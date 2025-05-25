92d87ae3272d3ae09d29ed6987df6b3725099541
darksider-05 <a.bahrkazemi84@gmail.com>
2025-05-25 13:29:42 +0330

    yahoo

---
+++ temp/yahoo-finance.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.firefox.options import Options
+import time
+
+options = Options()
+options.add_argument("--headless")
+
+driver = webdriver.Firefox()
+driver.get("https://finance.yahoo.com/")
+
+time.sleep(3)
+try:
+	var = WebDriverWait(driver, 10).until(
+		EC.presence_of_all_elements_located((By.TAG_NAME, "h3"))
+	)
+	for element in var:
+		print(element.text)
+except Exception as e:
+	print(f"{e}")
+
+
+
+driver.quit()
