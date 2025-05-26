ad3fafad6b8a25f488c2129c90a62bbac213322d
darksider-05 <a.bahrkazemi84@gmail.com>
2025-05-26 10:47:02 +0330

    no to GEMENI!!!

--- temp/bonbast.py
+++ temp/bonbast.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.firefox.options import Options
-
+dic = {}
 import time
 
 options = Options()
@@ -16,13 +16,38 @@ driver.get("https://www.bon-bast.com/")
 time.sleep(3)
 try:
 	var = WebDriverWait(driver, 10).until(
-		EC.presence_of_all_elements_located((By.TAG_NAME, "tr"))
+		EC.presence_of_all_elements_located((By.CSS_SELECTOR, "tr.active"))
 	)
-	for element in var:
-		print(element.text)
+	temp = []
+	for i in var:
+		temp.append(i.text)
+
+	var = temp[:]
 except Exception as e:
 	print(f"{e}")
 
+#try:
+#	var2 = WebDriverWait(driver, 10).until(
+#		EC.presence_of_all_elements_located((By.CLASS_NAME, "same_val"))
+#	)
+#	temp = []
+#	for i in var2:
+#		temp.append(i.text)
+#	
+#	var2 = temp[:]
+#except Exception as e:
+#	print(f"2:: {e}")
+
+#var2 = var2[6:]
+
+for element in var:
+	item = list(element.split(" "))
+	naame = f"{item[0]} : {item[1]}"
+	dic[naame] = { 'buy' :item[-1] , 'sell' :item[-2]}
+
+
+for i in dic:
+	print( f"{i}   :   {dic[i]["buy"]} , {dic[i]["sell"]}")
 
 
 driver.quit()
