c8e016139727217c5513319efe96f4dcfffad87b
alrz7 <alirezaarzcom7@gmail.com>
2025-05-29 20:35:08 +0330

    bonbast updated

--- services/Scrapers/bonbast.py
+++ services/Scrapers/bonbast.py
@@ -3,51 +3,48 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.firefox.options import Options
-dic = {}
+import random
 import time
 
-options = Options()
-options.add_argument("--headless")
 
-driver = webdriver.Firefox()
+def getcurrency():
+	driver = webdriver.Firefox()
+	driver.get("https://www.bon-bast.com/")
+	time.sleep(random.randint(5, 20))
+	driver.implicitly_wait(5)
+	memo = []
 
-driver.get("https://www.bon-bast.com/")
+	try:
+		var = WebDriverWait(driver, 10).until(
+			EC.presence_of_all_elements_located((By.CSS_SELECTOR, "table.table-condensed"))
+		)
+		for item in var:
+			memo.append((item.text))
+	except Exception as e:
+		print(f"{e}")
+	driver.quit()
+	
+	change = []
+	for num in 0,1:
+		for item in (memo[num].split('\n')):
+			change.append(item)
+		change.remove('Code Currency Sell Buy')
 
-time.sleep(3)
-try:
-	var = WebDriverWait(driver, 10).until(
-		EC.presence_of_all_elements_located((By.CSS_SELECTOR, "tr.active"))
-	)
-	temp = []
-	for i in var:
-		temp.append(i.text)
+	result = {}
 
-	var = temp[:]
-except Exception as e:
-	print(f"{e}")
+	for item_line in change:
+		parts = item_line.split(' ')
+		
+		if not parts or len(parts) < 3:
+			continue
+		code = parts[0]
+		buy = parts[-1]
+		sell = parts[-2]
+		name_parts = parts[1:-2]
+		name = " ".join(name_parts)
 
-#try:
-#	var2 = WebDriverWait(driver, 10).until(
-#		EC.presence_of_all_elements_located((By.CLASS_NAME, "same_val"))
-#	)
-#	temp = []
-#	for i in var2:
-#		temp.append(i.text)
-#	
-#	var2 = temp[:]
-#except Exception as e:
-#	print(f"2:: {e}")
+		result[code] = {'name': name, 'buy': buy, 'sell': sell}
 
-#var2 = var2[6:]
-
-for element in var:
-	item = list(element.split(" "))
-	naame = f"{item[0]} : {item[1]}"
-	dic[naame] = { 'buy' :item[-1] , 'sell' :item[-2]}
-
-
-for i in dic:
-	print( f"{i}   :   {dic[i]["buy"]} , {dic[i]["sell"]}")
+	return result
 
 
-driver.quit()
