c0f51f56534e1f50af4a4c02ce0003b052c8dec5
alirezaarz <alirezaarzcom7@gmail.com>
2025-07-29 00:46:46 +0330

    Add comments for testing analyze process in esdn.py

--- services/Scrapers/esdn.py
+++ services/Scrapers/esdn.py
@@ -135,3 +135,8 @@ a2 = {
 # -------test--------
 
 #analyze.az.sendtoQueue(a2, "esdn", "2025-07-27 01:16:29")
+
+# to test the analyze proccess just  uncomment the comment above and after a second comment it again real quick
+
+
+
