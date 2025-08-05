57f396f276cfc2fcff8c26afe8b2e8b50f8f1192
alirezaarz <alirezaarzcom7@gmail.com>
2025-08-06 00:15:34 +0330

    Refactor ecolog.py to remove unnecessary print statement and improve status output handling

--- services/management/commands/ecolog.py
+++ services/management/commands/ecolog.py
@@ -9,12 +9,15 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
         QueuePath = os.path.join(project_root, 'Data', 'analyze')
-        print(QueuePath)
         try:
             with open(os.path.join(QueuePath, "Status.json"), 'r', encoding='utf-8') as file:
                 log = json.load(file)
+                status = log["Status"]
+                if status:
+                    self.stdout.write(json.dumps(status, indent=4, ensure_ascii=False))
+                    
         except Exception as e:
             print(
                 f"there is an Error with Status.json; {e}")
-        self.stdout.write(json.dumps(log, indent=4, ensure_ascii=False))
+        
             
