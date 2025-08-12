370d82e11f1d241fe95ec08f3e6d22e66f3fbafe
alirezaarz <alirezaarzcom7@gmail.com>
2025-08-12 05:23:23 +0330

    sd

--- Ecovision/frontend/vite.config.js
+++ Ecovision/frontend/vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // or use '0.0.0.0'
-    port: 5173, // you can change this if needed
+    host: '127.0.0.1',
+    port: 5173,
   },
+  build: {
+    outDir: path.resolve(__dirname, 'dist'),
+    emptyOutDir: true
+  }
 })
